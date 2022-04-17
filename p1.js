3f97e2ddec106a09c96a8bee0fa7ec9ac563c621
shubham mishra <63717033+78640sam@users.noreply.github.com>
2022-04-17 23:24:40 +0530

    new Question

--- p1.js
+++ p1.js
@@ -1,14 +1,4 @@
-var arr=[1,2,3,4,5,6,7,8];
-out=""
-for(var i=0;i<arr.length;i++){
-if(i%2==0){
-  out+="ram"
-}esle if(i%3==0){
-out+="shyam"
-}else{
-  out+="mohan"
-}
-}
+
 
 function gele(n,arr){
     //console.log(n,arr)
