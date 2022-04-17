7a0c761371028edcea9656ede772f282844b8a5c
shubham mishra <63717033+78640sam@users.noreply.github.com>
2022-04-17 23:23:21 +0530

    update

--- p1.js
+++ p1.js
@@ -9,3 +9,20 @@ out+="shyam"
   out+="mohan"
 }
 }
+
+function gele(n,arr){
+    //console.log(n,arr)
+    var s=[];
+    var ans=[];
+    for(var i=n-1;i>=0;i--){
+        while(s.length>0&&s[s.length-1]<=arr[i]){
+            s.pop();
+        }
+      ans[i]=s.length>0 ? s[s.length-1]:-1;
+
+       s.push(arr[i]);
+    }
+   
+    
+    console.log(ans.join(" "));
+}
